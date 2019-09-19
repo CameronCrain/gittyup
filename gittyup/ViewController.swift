18091e62c5a314e0fff42da47a9ae8415e7f6f42
Cam Crain <43756114+CameronCrain@users.noreply.github.com>
2019-09-18 22:34:19 -0300

    Changed userName.

--- gittyup/ViewController.swift
+++ gittyup/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let userName = "dummyUser"
+    let userName = "smartUser"
 
     override func viewDidLoad() {
         super.viewDidLoad()
